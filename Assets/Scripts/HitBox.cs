45b67065c4bec9560ac946a7f5fa2c3d41eca10d
alsgh990710 <>
2024-03-21 21:57:53 +0900

    동기화

---
+++ Assets/Scripts/HitBox.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    public enum enumHitType
+    {
+        WallCheck,
+        ItemCheck,
+    }
+    [SerializeField] private enumHitType hitType;
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = transform.parent.GetComponent<Player>();
+        player = GetComponentInParent<Player>();//내 부모한테 있는게 확실하다묜
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.TriggerEnter(hitType, collision);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player.TriggerExit(hitType, collision);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
