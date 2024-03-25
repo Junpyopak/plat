8d0714fa3a36c6a71f2927b737e3f7f9a948b54b
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-03-26 00:39:06 +0900

    적의 이동 오류 해결

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float speed;
-
     Rigidbody2D rigid;
     Collider2D groundCheckCol;//땅을 체크하기위한
+    CapsuleCollider2D groundCheck; //enemy에 설정된 캡슐 콜라이더로 땅을 밟고있는지 확인을 위함
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         groundCheckCol = transform.GetChild(0).GetComponent<Collider2D>();
+        groundCheck = GetComponent<CapsuleCollider2D>();
+        
     }
 
     private void FixedUpdate()//defult,0.02초에 한번씩
     {
-        if (groundCheckCol.IsTouchingLayers(LayerMask.GetMask("Ground")) == false)
+        if(groundCheck.IsTouchingLayers(LayerMask.GetMask("Ground"))==true)//enemy에 설정된 캡슐 콜라이더로 땅을 밟고 있으면서
+                                                                           //땅체크하는 체크박스가 ground에서 떨어지면flip함수 실행(공중에 있을때 부들거리며 떨어지는걸 방지)
         {
-            flip();
+            if (groundCheckCol.IsTouchingLayers(LayerMask.GetMask("Ground")) == false)//땅 체크하는 체크박스가 ground에서 떨어지면flip함수 실행
+            {
+                flip();
+            }
         }
+            //if (groundCheckCol.IsTouchingLayers(LayerMask.GetMask("Ground")) == false)//땅 체크하는 체크박스가 ground에서 떨어지면flip함수 실행
+            //{
+            //    flip();
+            //}
+
     }
 
     private void flip()//땅이 없으면 뒤로 돌기
@@ -31,8 +42,10 @@ public class Enemy : MonoBehaviour
         transform.localScale = localScale;
     }
 
+
     private void Update()
     {
         rigid.velocity = new Vector2(speed,rigid.velocity.y);
+
     }
 }
